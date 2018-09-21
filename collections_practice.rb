5c9af4d969a6c3279dc298aafc29b0f4294d338a
Samuel Cornet <samuel@noukod.com>
2018-09-21 17:07:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,14 +46,3 @@ def sum_array(array)
   	 array.inject { |result, element| result += element }
 end
 
-def kesha_maker(array)
-
-  array.each_with_index.collect do |element, index|
-  if index == 1
-     element
-  else
-   element << "s"
-  end
-  end
-  # return array
-end
