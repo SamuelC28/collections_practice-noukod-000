af0b34f81f5a97ab32f61fb1aa8beb6b3c3b8d3d
Samuel Cornet <samuel@noukod.com>
2018-09-21 15:07:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,6 +7,11 @@ def sort_array_desc(array)
  array.sort_by { |number| -number }
 end
 
+def sort_array_char_count(array)
+  array.sort do |x, y| 
+    x.length <=> y.length
+end
+
 def swap_elements(array)
 swap_elements([array[0], array[1], array[2]],1,2)
 end
