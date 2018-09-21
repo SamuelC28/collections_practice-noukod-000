ff3020c494d8d87b91eb45b109571fe1a21acc6d
Samuel Cornet <samuel@noukod.com>
2018-09-21 14:59:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,7 @@ def sort_array_desc(array)
  #array.sort.reverse
  array.sort_by { |number| -number }
 end
+
+def swap_elements(array, index, destination)
+swap_elements([index[0], index[1], index[2],1,2)
+end
